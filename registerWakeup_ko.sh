951ebdb837b07ab9f8b5ff2dec0b6b71dca39914
ziro.park <ziro.park@navercorp.com>
2020-06-26 17:07:30 +0900

    fix bug

--- registerWakeup_ko.sh
+++ registerWakeup_ko.sh
@@ -2,4 +2,4 @@
 
 dirpath=`dirname $0`
 cd $dirpath
-./sleepwatcher_2.2.1/sleepwatcher -d -w "osascript ./airpods.scpt 연결"
+nohup ./sleepwatcher_2.2.1/sleepwatcher -w "osascript ./airpods.scpt 연결" &
